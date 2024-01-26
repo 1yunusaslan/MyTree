eb403e16c52be3a75133c8796589930c5474f570
Ahmet Akif Arslan <ahmetakifarslan@gmail.com>
2024-01-26 22:02:09 +0300

    find methods created

--- pages/js_pages/js/satir_stun.js
+++ pages/js_pages/js/satir_stun.js
@@ -1,56 +1,63 @@
+const tablo = [
+  ["*", 1, 8, 9, 10, 11],
+  ["*", 800, 64, 72, 80, 88],
+  ["*", 900, 72, 81, 90, 99],
+  ["*", 1000, 80, 90, 100, 110],
+];
 
-  const tablo = [
-    [1,8,9,10,11],
-    [800,64,72,80,88],
-    [900,72,81,90,99],
-    [1000,80,90,100,110]
-    
-  ];
-
-  const tablo2 = [
-    [1,2,2,3,1],
-    [1,3,2,1,4],
-    [2,4,3,4,1],
-    [3,5,4,5,2]
-    
-  ];
-
- 
- 
-  
-
-function findValuee (){
-    const satir_deger = document.getElementById("satir_number").value;
-    const sutun_deger = document.getElementById("sutun_number").value;
-    document.getElementById("sonuc").innerHTML= tablo[satir_deger][sutun_deger];
-};
-
-    //const snc = findValue(satir_deger,sutun_deger);
-    //console.log(snc);
-    //document.getElementById("sonuc").innerHTML=snc;
- 
-
-
-
-
-
-
-
-function sameValue (deger){
-    for (let i = 0; i < tablo2.length; i++) {
-        const satir = tablo2[i];
-        for (let k = 0; k < satir.length; k++) {
-            const sutun = satir[k];
-            if(satir===sutun){
-                return deger;
-            }
-        }
-        
-    }
-
+function getIntersection(satirno, sütunno) {
+  return tablo[satirno][sütunno];
 }
-const sonuc = sameValue(2);
-console.log(sonuc);
 
+function findItem(target) {
+  for (let index = 0; index < tablo.length; index++) {
+    const row = tablo[index];
 
-  document.getElementById("hesapla").onclick = findValuee;
+    for (let index = 0; index < row.length; index++) {
+      if (row[i] === target) {
+        return row[i];
+      }
+    }
+  }
+}
+
+const tablo2 = [
+  [1, 2, 2, 3, 1],
+  [1, 3, 2, 1, 4],
+  [2, 4, 3, 4, 1],
+  [3, 5, 4, 5, 2],
+];
+
+for (let i = 0; i < tablo2.length; i++) {
+  const satir = tablo2[i];
+  satir.find((eleman) => eleman == satir[sutunindex]);
+}
+
+function findValuee() {
+  const satir_deger = document.getElementById("satir_number").value;
+  const sutun_deger = document.getElementById("sutun_number").value;
+  document.getElementById("sonuc").innerHTML = tablo[satir_deger][sutun_deger];
+}
+
+//const snc = findValue(satir_deger,sutun_deger);
+//console.log(snc);
+//document.getElementById("sonuc").innerHTML=snc;
+
+// function sameValue (satirno, sutunno){
+//     let sütun = [];
+//     let satir = tablo2[satirno]
+//     for (let i = 0; i < tablo2.length; i++) {
+//         const satir = tablo2[i];
+//         sütun.push(satir[sutunno])
+//     }
+
+//     for (let i = 0; i < sütun.length; i++) {
+//         if(sütun[i] === satir[i]){
+
+//         }
+//     }
+// }
+// const sonuc = sameValue(2);
+// console.log(sonuc);
+
+document.getElementById("hesapla").onclick = findValuee;
